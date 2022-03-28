56a09cf2e2e1972266f9b2d2b764c2c00a0396c9
Antonov Kirill <antonovkir2@gmail.com>
2022-03-28 16:36:23 +0500

    refactor solution

--- JS solution/Define a card suit.js
+++ JS solution/Define a card suit.js
@@ -1,9 +1,13 @@
 function defineSuit(card) {
     const suit = card.slice(-1)
     switch (suit) {
-        case '♣': return 'clubs'
-        case '♦': return 'diamonds'
-        case '♥': return 'hearts'
-        case '♠': return 'spades'
+        case '♣':
+            return 'clubs'
+        case '♦':
+            return 'diamonds'
+        case '♥':
+            return 'hearts'
+        case '♠':
+            return 'spades'
     }
 }
