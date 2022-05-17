55ad459abe753169b6ed905f5bcc4eab4e610333
Antonov Kirill <antonovkir2@gmail.com>
2022-05-17 15:30:49 +0500

    task solution

--- JS solution/Cat years, Dog years.js
+++ JS solution/Cat years, Dog years.js
@@ -1,6 +1,6 @@
 //NOTES:
-    //humanYears >= 1
-    //humanYears are whole numbers only
+//humanYears >= 1
+//humanYears are whole numbers only
 
 //Cat Years
 //15 cat years for first year
@@ -12,7 +12,19 @@
 //+9 dog years for second year
 //+5 dog years for each year after that
 
-var humanYearsCatYearsDogYears = function(humanYears) {
-    // Your code here!
-    return [0,0,0];
+export const humanYearsCatYearsDogYears = (humanYears) => {
+
+    let catYears
+    let dogYears
+
+    if (humanYears === 1) {
+        dogYears = catYears = 15
+    } else if (humanYears === 2) {
+        dogYears = catYears = 15 + 9
+    } else {
+        catYears = (humanYears - 2) * 4 + 15 + 9
+        dogYears = (humanYears - 2) * 5 + 15 + 9
+    }
+
+    return [humanYears, catYears, dogYears];
 }
