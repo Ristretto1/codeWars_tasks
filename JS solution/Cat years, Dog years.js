59b1015d72a435c3f880021b9953008d3cc6d4d0
Antonov Kirill <antonovkir2@gmail.com>
2022-05-17 15:12:28 +0500

    add new task

---
+++ JS solution/Cat years, Dog years.js
@@ -0,0 +1,18 @@
+//NOTES:
+    //humanYears >= 1
+    //humanYears are whole numbers only
+
+//Cat Years
+//15 cat years for first year
+//+9 cat years for second year
+//+4 cat years for each year after that
+
+//Dog Years
+//15 dog years for first year
+//+9 dog years for second year
+//+5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    // Your code here!
+    return [0,0,0];
+}
