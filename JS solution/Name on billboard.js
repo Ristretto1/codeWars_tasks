e46d454728483d944e315f8488876dd1dc16e634
Antonov Kirill <antonovkir2@gmail.com>
2022-05-31 10:09:18 +0500

    task solution

--- JS solution/Name on billboard.js
+++ JS solution/Name on billboard.js
@@ -1,3 +1,9 @@
-function billboard(name, price = 30){
+function billboard(name, price = 30) {
+    let counter = price
 
+    for (let i = 1; i < name.length; i++) {
+        counter += price
+    }
+
+    return counter
 }
